f2216257b0dfc7f91f8134bbf1e0d000664d503d
Luis Felipe <luisfelipe@grupovdl.com.br>
2018-01-12 14:55:09 -0200

    Criação do Banco, Classe Dbcontext, e Controller Relatorios

---
+++ Relatorios_ASPNET_MVC/Controllers/RelatoriosController.cs
@@ -0,0 +1,49 @@
+﻿using PagedList;
+using Relatorios_ASPNET_MVC.Models;
+using Rotativa;
+using Rotativa.Options;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Relatorios_ASPNET_MVC.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        private VendaRelatorioDbContext _db = new VendaRelatorioDbContext();
+
+        // GET: Relatorios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListagemClientes(int? pagina, Boolean? gerarPDF)
+        {
+            var listagemClientes = _db.Clientes.OrderBy(n => n.ClienteId).ToList<Cliente>();
+
+            if(gerarPDF != true)
+            {
+                //Definindo a paginação
+                int paginaQtdeRegistros = 10;
+                int paginaNumeroNavegação = (pagina ?? 1);
+
+                return View(listagemClientes.ToPagedList(paginaNumeroNavegação, paginaQtdeRegistros));
+            }
+            else
+            {
+                int paginaNumero = 1;
+
+                var pdf = new ViewAsPdf
+                {
+                    ViewName = "ListagemClientes",
+                    PageSize = Size.A4,
+                    IsGrayScale = true,
+                    Model = listagemClientes.ToPagedList(paginaNumero, listagemClientes.Count)
+                };
+
+                return pdf;
+            }
+        }
+    }
+}
---
+++ Relatorios_ASPNET_MVC/Models/VendaRelatorioDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Entity;
+
+namespace Relatorios_ASPNET_MVC.Models
+{
+    public class VendaRelatorioDbContext : DbContext
+    {
+        static VendaRelatorioDbContext()
+        {
+            Database.SetInitializer<VendaRelatorioDbContext>(null);
+        }
+
+        public VendaRelatorioDbContext() : base("Name=VendaRelatorioDbContext")
+        {
+            
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<ItemPedido> ItemPedidos { get; set; }
+        public DbSet<PedidoCabecalho> PedidoCabecalhos { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ClienteMap());
+            modelBuilder.Configurations.Add(new ItemPedidoMap());
+            modelBuilder.Configurations.Add(new PedidoCabecalhoMap());
+            modelBuilder.Configurations.Add(new ProdutoMap());
+        }
+    }
+}
